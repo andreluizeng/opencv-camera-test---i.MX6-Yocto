dce0ed138e0f13a73ce84503e3cbda4b97692a9c
andre <andreluizeng@yahoo.com.br>
2014-09-22 09:42:40 -0300

    new gesture recognition demo
    Signed-off-by: andre <andreluizeng@yahoo.com.br>

--- src/camera_test.c
+++ src/camera_test.c
@@ -9,6 +9,10 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc_c.h"
 
+#include "libfreenect/libfreenect.h"
+
+#include <pthread.h>
+
 #include <time.h>
 #include <sys/time.h>
 
@@ -17,12 +21,59 @@
 
 #include <fcntl.h>
 
+
+#include <GLES2/gl2.h>
+#include <EGL/egl.h>
+#include <GLES2/gl2ext.h>
+#include <EGL/eglext.h>
+
+
 #define false 0
 #define true 1
 
 struct timeval start, end;
-long mtime, seconds, useconds;    
+long mtime, seconds, useconds;   
 
+
+/////// freenect vars
+uint16_t t_gamma[2048];
+pthread_t freenect_thread;
+volatile int die = 0;
+
+int depth_window;
+int video_window;
+
+pthread_mutex_t depth_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t video_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// back: owned by libfreenect (implicit for depth)
+// mid: owned by callbacks, "latest frame ready"
+// front: owned by GL, "currently being drawn"
+uint8_t *depth_mid, *depth_front;
+uint8_t *rgb_back, *rgb_mid, *rgb_front;
+
+GLuint gl_depth_tex;
+GLuint gl_rgb_tex;
+
+freenect_context *f_ctx;
+freenect_device *f_dev;
+int freenect_led;
+
+freenect_video_format requested_format = FREENECT_VIDEO_YUV_RGB;
+freenect_video_format current_format = FREENECT_VIDEO_YUV_RGB;
+freenect_resolution requested_resolution = FREENECT_RESOLUTION_MEDIUM;
+freenect_resolution current_resolution = FREENECT_RESOLUTION_MEDIUM;
+
+pthread_cond_t gl_frame_cond = PTHREAD_COND_INITIALIZER;
+int got_rgb = 0;
+int got_depth = 0;
+int depth_on = 1;
+
+
+void rgb_cb			(freenect_device *dev, void *rgb, uint32_t timestamp);
+void depth_cb			(freenect_device *dev, void *v_depth, uint32_t timestamp);
+void *freenect_threadfunc	(void *arg);
+//////////////////
 
 int Kbhit (void)
 {
@@ -64,6 +115,46 @@ int main(int argc, char *argv[])
 	IplImage *frame4;
 	
 	char key;
+	
+	
+	
+	
+	printf ("\nInitializing Kinect...");
+	if (freenect_init(&f_ctx, NULL) < 0) {
+		printf("Failed\n");
+		return 1;
+	}
+	printf ("OK\n");
+
+	freenect_set_log_level(f_ctx, FREENECT_LOG_DEBUG);
+	freenect_select_subdevices(f_ctx, (freenect_device_flags)(FREENECT_DEVICE_MOTOR | FREENECT_DEVICE_CAMERA));
+	
+	int nr_devices = freenect_num_devices (f_ctx);
+	printf ("\t\tNumber of devices found: %d\n", nr_devices);
+
+	int user_device_number = 0;
+	if (argc > 1)
+		user_device_number = atoi(argv[1]);
+
+	if (nr_devices < 1)
+		return 1;
+
+	if (freenect_open_device(f_ctx, &f_dev, user_device_number) < 0) {
+		printf("\t\tCould not open device\n");
+		return 1;
+	}
+	
+	int i;
+	for (i=0; i<2048; i++) {
+		float v = i/2048.0;
+		v = powf(v, 3)* 6;
+		t_gamma[i] = v*6*256;
+	}
+	
+	
+	
+	
+	
 	
 	capture1 = cvCaptureFromCAM (CV_CAP_ANY);
 
